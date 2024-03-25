db77479809159c2b08917f7a325e7c0354044546
hugooliveira72 <hugo72900@gmail.com>
2024-03-25 18:13:33 -0300

    [feature]: renomeando program para script

---
+++ playwright/Model/FormObject.cs
@@ -0,0 +1,18 @@
+﻿namespace playwright.Model
+{
+    public class FormObject
+    {
+        public string TxtboxUser { get; set; }
+        public string TxtboxPassword { get; set; }
+        public bool CheckboxWriteCredentials { get; set; }
+        public string TxtboxJob { get; set; }
+
+        public FormObject(dynamic formObject)
+        {
+            this.TxtboxUser = formObject.txtboxUser;
+            this.TxtboxPassword = formObject.txtboxPassword;
+            this.CheckboxWriteCredentials = formObject.checkboxWriteCredentials;
+            this.TxtboxJob = formObject.txtboxJob;
+        }
+    }
+}
