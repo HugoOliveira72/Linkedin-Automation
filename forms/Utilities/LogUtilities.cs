ff888d6c3dc324cc49ab6bd8e23cf51733d41d60
hugooliveira72 <hugo72900@gmail.com>
2024-03-27 22:26:00 -0300

    [feature]: Criando função writeStartMessage, para escrever mensagem inicial do programa

--- forms/Utilities/LogUtilities.cs
+++ forms/Utilities/LogUtilities.cs
@@ -1,15 +1,25 @@
-﻿namespace Linkedin_Automation.Utilities
+﻿using forms.Forms;
+using forms.Utilities.Messages;
+
+namespace Linkedin_Automation.Utilities
 {
     public class LogUtilities
     {
         private string logpath;
+        private StringPatterns stringPatterns = new StringPatterns();
+
         public LogUtilities()
         {
-            this.logpath = Script.LOGPATH;
+            this.logpath = RunningScreen.LOGPATH;
         }
         public void createLogFile()
         {
-            File.WriteAllText(this.logpath, $"============================\nExec: {DateTime.Now}");
+            File.WriteAllText(this.logpath, "");
+        }
+
+        public void writeStartMessage()
+        {
+            File.AppendAllText(this.logpath, Environment.NewLine + stringPatterns.startPattern());
         }
 
         public void writeError(string text)
