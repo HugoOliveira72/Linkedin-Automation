c5f6cfe70c3e2762c47780efba040d6a1cf2ea60
hugooliveira72 <hugo72900@gmail.com>
2024-03-27 22:28:28 -0300

    [feature]: adicionando mensagens novas

--- forms/Utilities/Messages/ExceptionMessages.cs
+++ forms/Utilities/Messages/ExceptionMessages.cs
@@ -5,5 +5,7 @@
         public const string InvalidData = "Dados incorretos!";
         public const string EmptyEmail = "'Email' é um endereço de email inválido.'Email' deve ser informado.";
         public const string EmptyPassword = "'Senha' deve ser informada!";
+        public const string IncorretLogin = "Login Incorreto!";
+        public const string SecurityError = "Erro na verificação de segurança";
     }
 }
