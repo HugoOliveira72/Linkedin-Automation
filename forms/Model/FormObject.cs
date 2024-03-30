e7af0d73211028a79dbe37320a0f7d74829c06ee
hugooliveira72 <hugo72900@gmail.com>
2024-03-30 16:11:06 -0300

    [feature]: criando extensions, adicionando campos novos em formObject

--- forms/Model/FormObject.cs
+++ forms/Model/FormObject.cs
@@ -1,4 +1,6 @@
-﻿namespace playwright.Model
+﻿using forms.Utilities;
+
+namespace playwright.Model
 {
     public class FormObject
     {
@@ -7,13 +9,20 @@
         public bool CheckboxWriteCredentials { get; set; }
         public string TxtboxJob { get; set; }
         public int AmoutOfJobs { get; set; }
-
+        public string? ClassifyBy { get; set; }
+        public List<string> CheckedListBoxExperiences { get; set; }
+        public List<string> CheckedListBoxType_job { get; set; }
+        public List<string> CheckedListBoxRemote { get; set; }
         public FormObject(
             string txtbox_user,
             string txtbox_password,
             bool checkbox_write_credentials_in_file,
             string txtbox_job,
-            int amount_jobs
+            int amount_jobs,
+            string classifyBy,
+            CheckedListBox.CheckedItemCollection ExperiencesCheckedItemCollection,
+            CheckedListBox.CheckedItemCollection TypeJobCheckedItemCollection,
+            CheckedListBox.CheckedItemCollection RemoteCheckedItemCollection
             )
         {
             this.TxtboxUser = txtbox_user;
@@ -21,6 +30,10 @@
             this.CheckboxWriteCredentials = checkbox_write_credentials_in_file;
             this.TxtboxJob = txtbox_job;
             this.AmoutOfJobs = amount_jobs;
+            this.ClassifyBy = classifyBy;
+            this.CheckedListBoxExperiences = ExperiencesCheckedItemCollection.ToList();
+            this.CheckedListBoxType_job = TypeJobCheckedItemCollection.ToList();
+            this.CheckedListBoxRemote = RemoteCheckedItemCollection.ToList();
         }
     }
 }
---
+++ forms/Utilities/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace forms.Utilities
+{
+    public static class Extensions
+    {
+        public static List<string> ToList(this CheckedListBox.CheckedItemCollection collection) => collection.Cast<object>().Select(item => item.ToString()).ToList();
+    }
+}
